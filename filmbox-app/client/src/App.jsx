7952deb29e7e566ae9fc7d78c7e8df227b302934
mmaanneell <150070696+mmaanneell@users.noreply.github.com>
2025-02-26 10:03:08 -0500

    modifications dans app, juste la premiere partie de ma page connexion

--- filmbox-app/client/src/App.jsx
+++ filmbox-app/client/src/App.jsx
@@ -3,8 +3,13 @@ import Login from './Account';
 
 function App() {
   return (
-    <div>
-      <Login />
+    <div className='login-container'>
+      <h2 className="form-title">Log in with</h2>
+      <div className="social-login"/>
+      <button className='social-button'>
+        <img src="assets/logoFilmBox.png" alt="FilmBox" className="social-icon" />
+        FilmBox
+      </button>
     </div>
   );
 }
