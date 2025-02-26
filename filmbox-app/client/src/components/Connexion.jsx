a23c159333d697edf862203ab0ab9c237cf64c7f
mmaanneell <150070696+mmaanneell@users.noreply.github.com>
2025-02-26 14:00:36 -0500

    modification du css pour que la page connexion soit plus belle

--- filmbox-app/client/src/components/Connexion.jsx
+++ filmbox-app/client/src/components/Connexion.jsx
@@ -6,36 +6,54 @@ import '../styles/Connexion.css';
 function Connexion(){
 return(
 
+  <body> 
+    <h2 className="form-title">Log in</h2>
+     
     <div className='login-container'>
-      <h2 className="form-title">Log in with</h2>
-
-
-      <div className="social-login">
-        <button className='social-button'>
-          <img src={logoFilmBox} className="social-icon" />
-        </button>
-      </div>
 
       <form action='#' className='login-form'>
+        {/* Email */}
+        <label htmlFor="email" className="input-label">Email</label>
+        
         <div className='input-wrapper'>
           <input type='email' placeholder='example@gmail.com' className='input-field' required ></input>
           <i><img src={arobase} alt="Arobase" className="icon-image" /> </i>
         </div>
 
+        {/* Mot de passe */}
+        <label htmlFor="password" className="input-label">Password</label>
+          
         <div className='input-wrapper'>
           <input type='password' placeholder='enter your password' className='input-field' required ></input>
           <i><img src={cadenas} alt="Cadenas" className="icon-image" /> </i>
         </div>
+
+        {/* Checkbox "Remember me" */}
+        <div className="remember-me">
+          <input type="checkbox" id="remember" />
+          <label htmlFor="remember"> Remember me</label>
+        </div>
+
+        {/* Bouton de connexion */}
+        <button type="submit" className="btn btn-primary login-btn">Login</button>
+
       </form>
 
+      {/* Don't have an account? */}
+      <div className='signup-container'>
+
       <p className='signup-text'>You don’t have an account? </p>
-        {/*Ceci (le bouton) vient de ChatGPT en attendant de trouver la solution moi même */}
       
+        {/*Ceci (le bouton) vient de ChatGPT en attendant de trouver la solution moi même */}
       <button className="btn btn-outline-primary" onClick={() => window.location.href = '/inscription'}>
         Create Account
       </button>
+      </div>
+
+
 
     </div>
+  </body>
 )
 }
 
