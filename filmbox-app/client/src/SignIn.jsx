42d0fecbf6a11964225f90f3b8a52063fb506a51
Alicia Ach <aali86362@outlook.com>
2025-02-28 23:17:18 -0500

    style pour 2 colonnes + name for each input box

--- filmbox-app/client/src/SignIn.jsx
+++ filmbox-app/client/src/SignIn.jsx
@@ -3,47 +3,54 @@ import './SignIn.css';
 
 function LoginRegister() {
     return (
+        <>
+    <h1>Sign Up</h1>
         <div className="wrapper">
             <div className="form-box login">
                 <form action="">
-                    <h1>Sign Up</h1>
                     
                     <div className="row">
                         <div className="input-box">
-                            <input type="text" placeholder="Ex: Tremblay" required />
+                            <p>First Name:</p>
+                            <input type="text" placeholder="Ex: Marie" required />
                         </div>
                         <div className="input-box">
-                            <input type="text" placeholder="Ex: Marie" required />
+                            <p>Last Name:</p>
+                            <input type="text" placeholder="Ex: Tremblay" required />
                         </div>
                     </div>
                     
                     <div className="row">
                         <div className="input-box">
+                            <p>Email:</p>
                             <input type="text" placeholder="Ex: marietremblay@gmail.com" required />
                         </div>
                         <div className="input-box">
+                            <p>Phone Number:</p>
                             <input type="text" placeholder="Ex: 514-123-5678" required />
                         </div>
                     </div>
                     
                     <div className="row">
                         <div className="input-box">
+                            <p>Password:</p>
                             <input type="password" placeholder="Enter your password" required />
                         </div>
                         <div className="input-box">
+                            <p>Confirm Password:</p>
                             <input type="password" placeholder="Enter your password again" required />
                         </div>
                     </div>
 
                     <button type="submit">CREATE ACCOUNT</button>
-
-                    <div className="LoginLink">
-                        <p>Already have an account ?</p>
-                        <button type="submit">LOGIN</button>
-                    </div>
                 </form>
             </div>
-        </div>
+            </div>
+            <div className="LoginLink">
+                <p>Already have an account ?</p>
+                <button type="submit">LOGIN</button>
+            </div>
+        </>
     );
 }
 
