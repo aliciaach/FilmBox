1249c68d4e88869bace960bf07b537cbe4a518e6
mmaanneell <150070696+mmaanneell@users.noreply.github.com>
2025-03-24 16:43:48 -0400

    enlevé les import ... css

--- filmbox-app/client/src/components/Connexion.jsx
+++ filmbox-app/client/src/components/Connexion.jsx
@@ -4,7 +4,6 @@ import logoFilmBox from '../assets/logoFilmBox.png';
 import arobase from '../assets/icone_arobase.png';
 import cadenas from '../assets/icone_cadenas.png';
 import titanicImage from '../assets/Titanic.png';
-import '../styles/Connexion.css';
 import { useNavigate } from 'react-router-dom';
 
 function Connexion(){
